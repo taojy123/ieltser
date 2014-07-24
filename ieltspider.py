0a1952c9ac5661b0cd005ee290ea9ecdb0d96229
Taojy <taojy0325@thundersoft.com>
2014-07-24 14:44:06 +0800

    Initially completed

--- ieltspider.py
+++ ieltspider.py
@@ -9,91 +9,200 @@ import traceback
 import time
 import shutil
 import hashlib
-
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-#opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), urllib2.ProxyHandler({'http':"10.239.120.37:911"}))
-opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 5.2) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1'),
-                     ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'), 
-                     ('Accept-Language', 'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3'), 
-                     ('Connection', 'keep-alive'),
-                     ]
-opener.addheaders.append( ('Accept-encoding', 'identity') )
-opener.addheaders.append( ('Referer', 'http://ielts.etest.net.cn/login') )
+import json
+import pprint
 
 
-def get_page(url, data=None):
-    resp = None
-    n = 0
-    while n < 3:
-        n = n + 1
+
+
+
+
+class User(object):
+    """docstring for User"""
+
+    AREA_MAP = {
+        u"北京"   : "11" ,
+        u"天津"   : "12" ,
+        u"河北"   : "13" ,
+        u"山西"   : "14" ,
+        u"内蒙古" : "15" ,
+        u"辽宁"   : "21" ,
+        u"吉林"   : "22" ,
+        u"黑龙江" : "23" ,
+        u"上海"   : "31" ,
+        u"江苏"   : "32" ,
+        u"浙江"   : "33" ,
+        u"安徽"   : "34" ,
+        u"福建"   : "35" ,
+        u"江西"   : "36" ,
+        u"山东"   : "37" ,
+        u"河南"   : "41" ,
+        u"湖北"   : "42" ,
+        u"湖南"   : "43" ,
+        u"广东"   : "44" ,
+        u"广西"   : "45" ,
+        u"海南"   : "46" ,
+        u"重庆"   : "50" ,
+        u"四川"   : "51" ,
+        u"贵州"   : "52" ,
+        u"云南"   : "53" ,
+        u"陕西"   : "61" ,
+        u"甘肃"   : "62" ,
+        u"新疆"   : "65" ,
+    }
+
+    def __init__(self, username="", password="", adminDate=""):
+        self.username = username
+        self.password = password
+        self.adminDate = adminDate
+        self.pwd_md5 = hashlib.md5(password).hexdigest()
+        self.userid = username
+        self.areas = ""
+        self.centers = []
+
+
+    @property
+    def login_url(self):
+        return "http://ielts.etest.net.cn/login"
+
+    @property
+    def query_url(self):
+        return "http://ielts.etest.net.cn/myHome/%s/queryTestSeats?queryMonths=%s&queryProvinces=%s" % (self.userid, self.adminDate[:7], self.areas)
+
+    @property
+    def confirm_url(self):
+        return "http://ielts.etest.net.cn/myHome/%s/createOrderConfirm" % self.userid
+
+    @property
+    def appoint_url(self):
+        return "http://ielts.etest.net.cn/myHome/%s/newAppointment" % self.userid
+
+    @property
+    def reg_url(self):
+        return "http://ielts.etest.net.cn/myHome/%s/registration" % self.userid
+
+
+    def add_center(self, centerNameCn):
+        # centerNameCn must be unicode
+        self.centers.append(centerNameCn)
+
+    def set_areas(self, areas_list):
+        rs = []
+        for name in areas_list:
+            aid = self.AREA_MAP[name]
+            rs.append(aid)
+        self.areas = ",".join(rs)
+
+
+
+
+
+users = []
+s = open("info.txt").read().strip().decode("gbk")
+parts = s.split("\n\n")
+for part in parts:
+    username, password, adminDate, areas, centers = part.split("\n")
+    areas = areas.split(",")
+    centers = centers.split(",")
+    user = User(username, password, adminDate)
+    user.set_areas(areas)
+    user.centers = centers
+    users.append(user)
+
+
+while users:
+    print "=============================================================="
+
+    for user in users:
+
         try:
-            resp = opener.open(url, data, timeout=5)
-            page = resp.read()
-            return page
+
+            time.sleep(1)
+
+            print "-------", user.username, '---------'
+
+            cj = cookielib.CookieJar()
+            #opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+            opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), urllib2.ProxyHandler({'http':"10.239.120.37:911"}))
+            opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 5.2) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1'),
+                                 ('Host', 'ielts.etest.net.cn'), 
+                                 ]
+
+            def get_page(url, data=None):
+                resp = None
+                n = 0
+                while n < 2:
+                    n = n + 1
+                    try:
+                        resp = opener.open(url, data, timeout=5)
+                        page = resp.read()
+                        return page
+                    except:
+                        #traceback.print_exc()
+                        print "Will try after 1 seconds ..."
+                        time.sleep(1)
+                        continue
+                raise "Get page failed"
+
+
+            p = get_page(user.login_url)
+            csrf = re.findall(r'name="CSRFToken" value="(.*)"', p)[0]
+            print csrf
+
+            formData = urllib.urlencode({'userId' : user.username,
+                                         'userPwd' : user.pwd_md5,
+                                         'checkImageCode' : "",
+                                         'CSRFToken' : csrf,
+                                         })
+            p = get_page(user.login_url, formData)
+
+            if "我的主页" not in p:
+                raise "登陆失败 可能需要验证码"
+
+            userid = re.findall(r'ID: </label> <span style=".*">(.*)</span>', p)[0]
+            user.userid = userid
+
+            csrfs = re.findall(r'name="CSRFToken" value="(.*)"', p)
+            if not csrfs:
+                p = get_page(user.reg_url)
+                csrfs = re.findall(r'name="CSRFToken" value="(.*)"', p)
+            csrf = csrfs[0]
+            print csrf, userid
+
+
+            print user.query_url
+            p = get_page(user.query_url)
+
+            p = json.loads(p)
+
+            rs = []
+            for ls in p.values():
+                rs += ls
+
+            for r in rs:
+                if r["adminDate"] == user.adminDate and r["centerNameCn"] in user.centers:
+                    print r["adminDate"], r["centerNameCn"],
+                    if r["optStatusEn"] != "Book seat":
+                        print r["optStatusCn"]
+                        continue
+
+                    seatGuid = r["seatGuid"]
+                    print u"可以预订", seatGuid
+                    formData = urllib.urlencode({'seatGuid' : seatGuid,
+                                                 'CSRFToken' : csrf,
+                                                 })
+                    p = get_page(user.confirm_url, formData)
+                    if "btnConfirmNext" not in p:
+                        print u"确认失败 下一轮重试"
+                        continue
+                    p = get_page(user.appoint_url, formData)
+                    if '<li class="active">预订考位成功</li>' not in p:
+                        print u"预订失败 下一轮重试"
+                        continue
+                    print u"预订考位成功", username, r["adminDate"], r["centerNameCn"]
+                    success_msg = username + u" " + r["adminDate"] + r["centerNameCn"] + u"\n"
+                    open("success.txt", "a").write(success_msg.encode(gbk))
+                    users.remove(user)
+
         except:
             traceback.print_exc()
-            print "Will try after 1 seconds ..."
-            time.sleep(1)
-            continue
-        break
-    return "Null"
-
-
-AREA_MAP = {
-    "北京"   : "11" ,
-    "天津"   : "12" ,
-    "河北"   : "13" ,
-    "山西"   : "14" ,
-    "内蒙古" : "15" ,
-    "辽宁"   : "21" ,
-    "吉林"   : "22" ,
-    "黑龙江" : "23" ,
-    "上海"   : "31" ,
-    "江苏"   : "32" ,
-    "浙江"   : "33" ,
-    "安徽"   : "34" ,
-    "福建"   : "35" ,
-    "江西"   : "36" ,
-    "山东"   : "37" ,
-    "河南"   : "41" ,
-    "湖北"   : "42" ,
-    "湖南"   : "43" ,
-    "广东"   : "44" ,
-    "广西"   : "45" ,
-    "海南"   : "46" ,
-    "重庆"   : "50" ,
-    "四川"   : "51" ,
-    "贵州"   : "52" ,
-    "云南"   : "53" ,
-    "陕西"   : "61" ,
-    "甘肃"   : "62" ,
-    "新疆"   : "65" ,
-}
-
-LOGIN_URL = "http://ielts.etest.net.cn/login"
-QUERY_URL = "http://ielts.etest.net.cn/myHome/1945505/queryTestSeats?queryMonths=$qms&queryProvinces=$qps"
-
-username = "1945505"
-password = "huangliming" #"4a788c684c43ad35f11c3f5e2b39638e"
-pwd_md5 = hashlib.md5(password).hexdigest()
-
-p = get_page(LOGIN_URL)
-csrf = re.findall(r'name="CSRFToken" value="(.*)"', p)[0]
-print csrf
-
-formData = urllib.urlencode({'userId' : username,
-                             'userPwd' : pwd_md5,
-                             'checkImageCode' : "",
-                             'CSRFToken' : csrf,
-                             })
-p = get_page(LOGIN_URL, formData)
-if "我的主页" not in p:
-    raise "请输入验证码"
-#print p
-
-
-query_url = QUERY_URL.replace("$qms", "2014-09").replace("$qps", "11,12")
-print query_url
-p = get_page(query_url)
-print p
